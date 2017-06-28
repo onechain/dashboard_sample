f9de04662e57082ce1d7c6bb642f3e864d38ab28
gcc2ge <1414786333@qq.com>
2017-06-28 16:22:04 +0800

    init

--- js/widgets/metrix_block_min.js
+++ js/widgets/metrix_block_min.js
@@ -1,41 +1,45 @@
+import client from '../vendor/client';
 
 module.exports = function(id) {
 	var extended = {
 		name: 'metrix_block_min',
-		title: 'block min',
+		title: 'Blocks/min',
 		size: 'medium',
 		widgetId: id, //needed for dashboard
 
 		hideLink: true,
+        topic: '/topic/metrics/blocksPerMin',
 
-
+        subscribe: function() {
+            Client.subscribe(this.topic, this.onData);
+        },
 
 		fetch: function() {
 			$('#widget-' + widget.shell.id).html( '<div id="' + widget.name + '" class="epoch category10" style="width:100%; height: 210px;"></div>' );
 
 			widget.chart = $('#' + widget.name).epoch({
 				type: 'time.area',
-
-				data: [
-					// The first layer
-					{
-						label: "Layer 1",
-						values: [ {time: 1370044800, y: 100}, {time: 1370044801, y: 1000}, ]
-					},
-
-					// The second layer
-					{
-						label: "Layer 2",
-						values: [ {time: 1370044800, y: 78}, {time: 1370044801, y: 98},]
-					},
-
-				],
+                data: [ {
+                    label: 'Blocks per MIN',
+                    values: [ { time: (new Date()).getTime() / 1000, y: 0 } ]
+                } ],
 				axes: ['left', 'right', 'bottom']
 			});
 		},
 
 		onData: function(data) {
+			console.info(data)
+            /*data = data.data.attributes;
 
+            if (!data || !data.result) {
+                return;
+            }
+
+            var b = {
+                time: data.result.timestamp,
+                y: data.result.value
+            };
+            widget.chart.push([ b ]);*/
 		},
 	};
 
